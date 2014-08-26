77a6a3c34162e37893385ddbeb865fb935aa2134
VISHAL TALWAR <machome@vishal-mac-mini.gateway.2wire.net>
2014-08-26 00:22:53 -0700

    Code updated to get meetup events

--- public/javascripts/app/EventsMashup.js
+++ public/javascripts/app/EventsMashup.js
@@ -1,6 +1,6 @@
 (function($, _) {
     $(document).ready(function(){
-        $( ".mainContent" ).html("jQuery is working!");
+        $( ".techStack" ).html("jQuery - " + $().jquery + " | underscore - " + _.VERSION);
         
         if ("geolocation" in navigator) {
             console.log("geolocation IS available");
@@ -14,56 +14,11 @@
             
             getEvents(queryParams);
         }
-        
-        console.log("underscore version - " + _.VERSION);
-        
-        var tpl = _.template("<h1>Some text: <%= foo %></h1>");
-        $( "h1" ).after( tpl({foo: "blahblah"}) );
-        
-        /*
-        // When rending an underscore template, we want top-level
-        // variables to be referenced as part of an object. For
-        // technical reasons (scope-chain search), this speeds up
-        // rendering; however, more importantly, this also allows our
-        // templates to look / feel more like our server-side
-        // templates that use the rc (Request Context / Colletion) in
-        // order to render their markup.
-        _.templateSettings.variable = "rc";
-        
-        // Grab the HTML out of our template tag and pre-compile it.
-        var template = _.template( $( "script.template" ).html() );
- 
-        // Define our render data (to be put into the "rc" variable).
-        var templateData = {
-                            listTitle: "Olympic Volleyball Players",
-                            listItems: [
-                                {
-                                    name: "Misty May-Treanor",
-                                    hasOlympicGold: true
-                                },
-                                {
-                                    name: "Kerri Walsh Jennings",
-                                    hasOlympicGold: true
-                                },
-                                {
-                                    name: "Jennifer Kessy",
-                                    hasOlympicGold: false
-                                },
-                                {
-                                    name: "April Ross",
-                                    hasOlympicGold: false
-                                }
-                            ]
-                        };
-         
-        // Render the underscore template and inject it after the H1
-        // in our current DOM.
-        $( "h1" ).after( template( templateData ) );
-        */
     }); 
     
     function getEvents(queryParams) {
         console.log("getEvents() - queryParams: %o", queryParams );
+        
         $.ajax({
             type: "GET",
             url: "http://api.meetup.com/2/open_events",
@@ -71,10 +26,13 @@
             dataType: "jsonp",
             success: function(data) {
                 console.log("data: %o", data);
+                console.log("Meetup names: %o", _.pluck(data.results, 'name'));
+                var template = $("#eventList").html();
+                $( ".errorNode" ).after( _.template(template, {events:data.results}) );
             },
             error: function(XMLHttpRequest, textStatus, errorThrown) {
                 console.log("error - %o", { XMLHttpRequest: XMLHttpRequest, textStatus: textStatus, errorThrown: errorThrown });
-                $( ".mainContent" ).html(errorThrown);
+                $( ".errorNode" ).html(errorThrown);
             }
         });
     }
